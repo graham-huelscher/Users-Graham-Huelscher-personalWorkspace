df6bb1d52a932123ccfe2679753c9b13d9ef1f26
graham-huelscher <graham.huelscher@gmail.com>
2019-02-14 02:11:42 -0800

    cleaned up main server

--- angular-tour-of-heroes/backend/server.js
+++ angular-tour-of-heroes/backend/server.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || process.argv[2] || 8080
 const bodyParser = require('body-parser')
-const HeroesDB = require('./model/Hero')
 
 const cors = require('cors')
 const corsOptions = {
@@ -22,30 +21,3 @@ app.listen(port, () => {
   console.log(`listening on ${port}`)
 })
 
-// const Sequelize = require('sequelize');
-// const db = new Sequelize('HeroesDB', 'sa', 'sql2017', {
-//   host: 'localhost',
-//   dialect: 'mssql',
-//   operatorsAliases: false,
-
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     acquire: 30000,
-//     idle: 10000
-//   }
-// });
-
-// const Hero = db.define('Heroes', {
-//   Id: {
-//     primaryKey: true,
-//     type: Sequelize.INTEGER
-//   },
-//   Name: {
-//     type: Sequelize.STRING
-//   }
-// }, { timestamps: false, });
-
-HeroesDB.findAll({raw: true}).then(heroes => {
-  console.log(heroes)
-})
